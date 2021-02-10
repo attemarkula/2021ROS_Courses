b01eaaae4c78ac3b6abf984d5fa9e71b0562290f
Atte Markula <atte.markula@gmail.com>
2021-02-10 19:44:15 +0200

    Piiloita alusta Tensorflow varoitukset

---
+++ tests/tf-test3-warnings.py
@@ -0,0 +1,5 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+import tensorflow as tf
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0' 
+print ("TF ei anna alun virheitä, mutta ilmoittaa myöhemmistä.")
