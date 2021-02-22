1c4c759371f3a9d084110a3e6bea5b8f5d019f66
attemarkula <66369715+attemarkula@users.noreply.github.com>
2021-02-22 14:23:15 +0200

    korjaus models hakemiston liittämiseen
    Toimii aurinkoisella säällä.
    mikäli tarpeen, mutta tarpeen tullen mahdollista nollata variable alla olevalla komennolla:
    export $GAZEBO_MODEL_PATH=""

--- fixes/setup.bash
+++ fixes/setup.bash
@@ -3,6 +3,12 @@
 
 CATKIN_SHELL=bash
 
+#ei hyvä, toimii kun ei sada
+#jos liian pitkä, nollaa export $GAZEBO_MODEL_PATH=""
+_WS_LCAL_MODLS=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" > /dev/null && pwd)
+_WS_LCAL_MODLS="$_WS_LCAL_MODLS/../src/samk_robowar_world/models"
+export GAZEBO_MODEL_PATH=$_WS_LCAL_MODLS:$GAZEBO_MODEL_PATH
+
 # source setup.sh from same directory as this file
 _CATKIN_SETUP_DIR=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" > /dev/null && p>
 . "$_CATKIN_SETUP_DIR/setup.sh"
