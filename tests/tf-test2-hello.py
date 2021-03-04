52b955151bc72e527afbd3981f228d99ef8062b4
Atte Markula <atte.markula@gmail.com>
2021-03-04 17:33:21 +0200

    luento2 tiedostot pois

--- tests/tf-test2-hello.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-msg=tf.constant("Hello World, it works!")
-#print(msg)
-
-tf.print(msg)
--- tests/tf-test3-warnings.py
+++
@@ -1,5 +0,0 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
-import tensorflow as tf
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0' 
-print ("TF ei anna alun virheitä, mutta ilmoittaa myöhemmistä.")
